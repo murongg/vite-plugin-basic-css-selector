c18c01643303e677fd59e858568cf0a931b548bd
木荣 <372638156@qq.com>
2023-08-30 15:16:51 +0800

    feat: add apply option

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import { transform } from './transform'
 
 export interface Options {
   enforce?: Plugin['enforce']
+  apply?: Plugin['apply']
 }
 
 function VitePluginBasicCssSelector(basic: string, options?: Options): Plugin {
   return {
     name: 'vite-plugin-basic-css-selector',
     enforce: options?.enforce,
+    apply: options?.apply,
     transform(code, id) {
       return transform(code, id, basic)
     },
