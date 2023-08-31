35ce1d0e3c5b71d73178baede120dda1bbdd04a0
木荣 <372638156@qq.com>
2023-08-31 15:45:34 +0800

    feat: use js-beautify

--- src/format.ts
+++ src/format.ts
@@ -1,10 +1,8 @@
+import { css_beautify } from 'js-beautify'
+
 export function cssFormat(code: string, type: 'tab' | 'space' = 'space', indent = 2) {
-  const indentation = (type === 'tab' ? '\t' : '\ ').repeat(indent)
-  code = code.replace(/\s*([\{\}\:\;\,])\s*/g, '$1')
-  code = code.replace(/;\s*;/g, ';')
-  code = code.replace(/\,[\s\.\#\d]*{/g, '{')
-  code = code.replace(/([^\s])\{([^\s])/g, `$1 {\n${indentation}$2`)
-  code = code.replace(/([^\s])\}([^\n]*)/g, '$1\n}\n\n$2')
-  code = code.replace(/([^\s]);([^\s\}])/g, `$1;\n${indentation}$2`)
-  return code
+  return css_beautify(code, {
+    indent_size: indent,
+    indent_with_tabs: type === 'tab',
+  })
 }
